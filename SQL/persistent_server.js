f475afe41a40afa1f66d7ba07dcc100a2c92ccaa
Hack Reactor Students <sjsnider@gmail.com>
2014-05-09 21:57:55 -0700

    Update GET and POST to connect to SQL database

--- SQL/persistent_server.js
+++ SQL/persistent_server.js
@@ -6,8 +6,7 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
-  password: "",
+  user: "root",
   database: "chat"
 });
 
@@ -19,3 +18,27 @@ dbConnection.connect();
 
 /* You already know how to create an http server from the previous
  * assignment; you can re-use most of that code here. */
+/* Import node's http module: */
+var http = require("http");
+var request = require("./request-handler.js");
+
+/* Every server needs to listen on a port with a unique number. The
+ * standard port for HTTP servers is port 80, but that port is
+ * normally already claimed by another server and/or not accessible
+ * so we'll use a higher port number that is not likely to be taken: */
+var port = 3000;
+
+/* For now, since you're running this server on your local machine,
+ * we'll have it listen on the IP address 127.0.0.1, which is a
+ * special address that always refers to localhost. */
+var ip = "127.0.0.1";
+
+
+
+/* We use node's http module to create a server. Note, we called it 'server', but
+we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
+will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
+Lastly, we tell the server we made to listen on the given port and IP. */
+var server = http.createServer(request.handler);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
